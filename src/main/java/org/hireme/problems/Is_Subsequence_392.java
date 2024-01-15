66e38812b9b0b56889b6b7fa2b23a2e020a7dafa
Ezio99 <vaibhavritam@gmail.com>
2024-01-15 18:41:03 +0800

    1 ms solution

---
+++ src/main/java/org/hireme/problems/Is_Subsequence_392.java
@@ -0,0 +1,34 @@
+package org.hireme.problems;
+
+
+public class Is_Subsequence_392 {
+
+
+    //1 ms
+    public static boolean isSubsequence(String s, String t) {
+
+        if(s.isEmpty()){
+            return true;
+        }
+
+        int pos=0;
+
+        for(int i=0;i<t.length();i++){
+            if(t.charAt(i) == s.charAt(pos)){
+                if(pos>=s.length()-1){
+                    pos++;
+                    break;
+                }
+                pos++;
+            }
+        }
+
+        return pos > s.length()-1;
+    }
+
+
+
+    public static void main(String[] args){
+        System.out.println(isSubsequence("abc","ahbgdc"));
+    }
+}
