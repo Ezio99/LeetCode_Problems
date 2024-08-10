c440cfcff2b5887a7fd85a340a9faef015a2a2ab
Ezio99 <vaibhavritam@gmail.com>
2024-08-10 16:59:34 +0800

    Union Find

---
+++ src/main/java/org/hireme/dynamicProblems/easy/Find_If_Path_Exists_Graph.java
@@ -0,0 +1,77 @@
+package org.hireme.dynamicProblems.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/*
+    Union Find
+ */
+public class Find_If_Path_Exists_Graph {
+
+    List<Integer> id = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+
+        if (n < 1) {
+            return false;
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            id.add(i);
+            size.add(1);
+        }
+
+        for (int i = 0; i < edges.length; i++) {
+            union(edges[i][0],edges[i][1]);
+        }
+
+        return isConnected(source,destination);
+
+
+
+
+    }
+
+
+    private int findRoot(int x) {
+
+        int root = x;
+        while (id.get(root) != root) {
+            root = id.get(root);
+        }
+
+        int q = x, next;
+        while (id.get(q) != root) {
+            next = id.get(q);
+            id.set(q, root);
+            q = next;
+        }
+
+        return root;
+
+    }
+
+    private boolean isConnected(int x, int y){
+        return findRoot(x) == findRoot(y);
+    }
+
+    private void union(int x,int y){
+        int rootX = findRoot(x);
+        int rootY = findRoot(y);
+
+        if(rootX == rootY)
+            return;
+
+        if(size.get(rootX) >= size.get(rootY)){
+            id.set(rootY,rootX);
+            size.set(rootX,size.get(rootX)+size.get(rootY));
+        }
+        else{
+            id.set(rootX,rootY);
+            size.set(rootY,size.get(rootX)+size.get(rootY));
+        }
+    }
+}
