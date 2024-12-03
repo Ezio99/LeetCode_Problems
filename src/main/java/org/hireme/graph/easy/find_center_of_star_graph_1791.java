94d6f97c90c83a311c5ef414ef6eb7a4586bcee1
Ezio99 <vaibhavritam@gmail.com>
2024-12-03 15:21:08 +0800

    Optimized solution for find_center_of_star_graph_1791

--- src/main/java/org/hireme/graph/easy/find_center_of_star_graph_1791.java
+++ src/main/java/org/hireme/graph/easy/find_center_of_star_graph_1791.java
@@ -7,24 +7,17 @@ import java.util.List;
 public class find_center_of_star_graph_1791 {
 
     public static int findCenter(int[][] edges) {
-        int numOfEdges = edges.length;
-        HashMap<Integer, List<Integer>> edgesPerNode = new HashMap<>();
 
-        for (int[] edgePair : edges) {
-
-            edgesPerNode.computeIfAbsent(edgePair[0],k -> new ArrayList<>()).add(edgePair[1]);
-            edgesPerNode.computeIfAbsent(edgePair[1],k -> new ArrayList<>()).add(edgePair[0]);
-
-            if(edgesPerNode.get(edgePair[0]).size()==numOfEdges){
-                return edgePair[0];
-            }
-
-            if(edgesPerNode.get(edgePair[1]).size()==numOfEdges){
-                return edgePair[1];
-            }
-
+        // Star graph only has edges between the centre and other nodes
+        // Using that, the common node between 2 edges is the centre
+        if(edges[0][0]==edges[1][0] ||edges[0][0]==edges[1][1]){
+            return edges[0][0];
         }
-        return 0;
+        else{
+            return edges[0][1];
+        }
+
+
     }
 
     public static void main(String[] args) {
@@ -33,6 +26,7 @@ public class find_center_of_star_graph_1791 {
                 {2, 3},
                 {4, 2}
         };
+
 
         // Call the findCenter method
         int center = findCenter(edges);
